ad1cb23f5d510db32ad5253253471da0183aca94
Romain Wirth <wirth.romain@gmail.com>
2023-07-12 15:08:28 +0200

    UPD class character

--- src/fr/campus/dungeon/characters/Character.java
+++ src/fr/campus/dungeon/characters/Character.java
@@ -1,4 +1,37 @@
 package fr.campus.dungeon.characters;
 
 public class Character {
+    private String name;
+    private int healthPoints;
+    private int strengh;
+
+    public Character(String name, int heathPoints, int strengh) {
+        this.name = name;
+        this.healthPoints = heathPoints;
+        this.strengh = strengh;
+    }
+
+    public Character() {
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return healthPoints;
+    }
+    public void setHealth(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    public int getStrengh() {
+        return strengh;
+    }
+    public void setStrengh(int strengh) {
+        this.strengh = strengh;
+    }
 }
